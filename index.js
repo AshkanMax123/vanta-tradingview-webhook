04772717e9186a39a9bb92524f8e775cfffff146
AshkanMax123 <anthony.dianaty@gmail.com>
2025-10-08 21:56:15 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 
-app.post('/webhook', (req, res) => {
-  console.log('Webhook received:', req.body);
-  res.status(200).send('Webhook received');
-});
-
-app.get('/', (req, res) => {
-  res.send('Vanta TradingView Webhook is live!');
+app.post('/', (req, res) => {
+  console.log('Alert received:', req.body);
+  res.status(200).send('Webhook received!');
 });
 
 app.listen(port, () => {
